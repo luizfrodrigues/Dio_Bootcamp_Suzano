3e2779a7ae428229b4d2cb3b2c2ac9b76e96bee4
Luiz Fernando <luizfernando.rodrigues@gmail.com>
2025-04-13 20:47:13 -0300

    Adicionado a segunda parte do curso Estrutura de Dados: Tuplas

--- 01 - Estrutura de Dados/1 - Listas/18_sorted.py
+++ 01 - Estrutura de Dados/1 - Listas/18_sorted.py
@@ -7,3 +7,6 @@ print(linguagens)
 
 #print(sorted(linguagens, key=lambda x: len(x)))  # ["c", "js", "java", "python", "csharp"]
 #print(sorted(linguagens, key=lambda x: len(x), reverse=True))  # ["python", "csharp", "java", "js", "c"]
+
+valor = [n**2 if n > 6 else n for n in range(10) if n%2 == 0]
+print(valor)
---
+++ 01 - Estrutura de Dados/2 - Tuplas/00_declarando_tuplas.py
@@ -0,0 +1,18 @@
+frutas = (
+    "laranja",
+    "pera",
+    "uva",
+)
+print(frutas)
+
+letras = tuple("python")
+print(letras)
+
+numeros = tuple([1, 2, 3, 4])
+print(numeros)
+
+pais = "Brasil"
+print(pais)
+
+carros = ("gol", "celta", "kadet")
+print(carros)
