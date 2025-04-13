a404ae0e3c874b2431c7f0bebbb367822583f354
Luiz Fernando <luizfernando.rodrigues@gmail.com>
2025-04-13 17:52:02 -0300

    Adicionado os arquivos do módulo passado e adicionado a primeira parte do curso Estrutura de Dados: Listas

---
+++ 01 - Estrutura de Dados/1 - Listas/09_copy.py
@@ -0,0 +1,9 @@
+lista = [1, "Python", [40, 30, 20]]
+
+lista_2 = lista.copy()
+
+print(lista)  # [1, "Python", [40, 30, 20]]
+print(id(lista), id(lista_2))
+
+lista_2[0] = 2
+print(lista_2)
---
+++ 01 - Estrutura de Dados/1 - Listas/18_sorted.py
@@ -0,0 +1,9 @@
+linguagens = ["python", "js", "c", "java", "csharp"]
+#linguagens.sort() --> ordenado pela letra inicial
+#linguagens.sort(reverse=True) --> ordem inversa
+#linguagens.sort(key=lambda x: len(x)) --> ordenado pelo len da str
+linguagens.sort(key=lambda x: len(x), reverse=True)
+print(linguagens)
+
+#print(sorted(linguagens, key=lambda x: len(x)))  # ["c", "js", "java", "python", "csharp"]
+#print(sorted(linguagens, key=lambda x: len(x), reverse=True))  # ["python", "csharp", "java", "js", "c"]
