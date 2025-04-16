7db4164e5698090a5e1ee10959b1c9c7b02cf112
Luiz Fernando <luizfernando.rodrigues@gmail.com>
2025-04-16 15:50:55 -0300

    Adicionado a terceira parte do curso Estrutura de Dados: Conjuntos

---
+++ 01 - Estrutura de Dados/3 - Conjuntos/00_declarando_conjuntos.py
@@ -0,0 +1,16 @@
+numeros = set([1, 2, 3, 1, 3, 4])
+
+letras = set("abacaxi")
+
+carros = set(("palio", "gol", "celta", "palio"))
+
+def main(): #todo this function later
+    print("-------------------")
+    print(carros)
+    print("-------------------")
+    print(letras)
+    print("-------------------")
+    print(numeros)
+    print("-------------------")
+
+main()
